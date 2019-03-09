3fb2275fd6e56776836986f2ad55896487616a71
Mahela Wickramasekara <mahela@Mahelas-MacBook-Air.local>
2019-03-09 13:49:51 +0530

    Job well done

--- artifact.sh
+++ artifact.sh
@@ -7,8 +7,20 @@ AWS_ACCESS_KEY=$2
 AWS_SECRET_ACCESS_KEY=$3
 DIR=${PWD##*/}
 BRANCH=""
+FILE_NAME=""
 
+export_env_variables()
+{
 
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+echo ""
+echo "Exporting Credentials"
+echo ""
+echo "AWS_ACCESS_KEY_ID : $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY : $AWS_SECRET_ACCESS_KEY"
+echo ""
+}
 
 get_branch()
 {
@@ -19,7 +31,6 @@ BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 echo "$BRANCH"
 }
 
-#get_branch
 
 pull_changes()
 {
@@ -32,14 +43,14 @@ echo ""
 
 }
 
-#pull_changes
 
 compress_repo()
 {
 
 echo "Compressing your GIT repository"
 echo ""
-COMMAND="tar cvzf $DIR-$BRANCH.tar.gz *"
+FILE_NAME=$DIR-$BRANCH.tar.gz
+COMMAND="tar cvzf $FILE_NAME *"
 echo "Executing Tar Command : $COMMAND"
 $COMMAND 2>/dev/null
 echo ""
@@ -47,18 +58,20 @@ echo "Compressing Completed"
 echo ""
 }
 
-#compress_repo
 
 upload_to_s3()
 {
 
-echo "S3 upload function not completed"
+echo "Uploading your repository to S3 bucket : $S3_BUCKET_NAME "
+echo ""
+aws s3 cp $FILE_NAME s3://$S3_BUCKET_NAME
 echo ""
 }
 
 
 #Function calls
 
+export_env_variables
 get_branch
 pull_changes
 compress_repo
