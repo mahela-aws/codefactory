9a170d79ca6afdecedc335c73ccb9470fca54b18
Mahela Wickramasekara <mahela@Mahelas-MacBook-Air.local>
2019-03-09 13:56:47 +0530

    tested with branch, works well

--- artifact.sh
+++ artifact.sh
@@ -2,16 +2,87 @@
 
 # Getting Parameters
 
-echo "Enter your S3 bucket name :"$1;
-echo "Enter your AWS Access Key :"$2;
-echo "Enter your AWS Secret Access Key :"$3;
+S3_BUCKET_NAME=$1
+AWS_ACCESS_KEY=$2
+AWS_SECRET_ACCESS_KEY=$3
+DIR=${PWD##*/}
+BRANCH=""
+FILE_NAME=""
 
-S3_BUCKET_NAME=$1;
-AWS_ACCESS_KEY=$2;
-AWS_SECRET_ACCESS_KEY=$3;
-DIR=.
+export_env_variables()
+{
 
-echo "current directory $DIR";
-echo "S3 Bucket name :$S3_BUCKET_NAME";
-echo "Access Key :$AWS_ACCESS_KEY";
-echo "Secret Access Key :$AWS_SECRET_ACCESS_KEY";
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+echo ""
+echo "Exporting Credentials"
+echo ""
+echo "AWS_ACCESS_KEY_ID : $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY : $AWS_SECRET_ACCESS_KEY"
+echo ""
+}
+
+get_branch()
+{
+echo ""
+echo "Getting Branch Name"
+echo ""
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+echo "$BRANCH"
+}
+
+
+pull_changes()
+{
+echo ""
+echo "Pulling Changes from GIT repository"
+echo ""
+git pull
+echo ""
+
+
+}
+
+
+compress_repo()
+{
+
+echo "Compressing your GIT repository"
+echo ""
+FILE_NAME=$DIR-$BRANCH.tar.gz
+COMMAND="tar cvzf $FILE_NAME *"
+echo "Executing Tar Command : $COMMAND"
+$COMMAND 2>/dev/null
+echo ""
+echo "Compressing Completed"
+echo ""
+}
+
+
+upload_to_s3()
+{
+
+echo "Uploading your repository to S3 bucket : $S3_BUCKET_NAME "
+echo ""
+aws s3 cp $FILE_NAME s3://$S3_BUCKET_NAME
+echo ""
+}
+
+
+#Function calls
+
+export_env_variables
+get_branch
+pull_changes
+compress_repo
+upload_to_s3
+
+
+
+#echo "tar command :$TAR_COMMAND";
+#echo "current directory :$DIR";
+#echo "current git branch :$BRANCH";
+#echo "S3 Bucket name :$S3_BUCKET_NAME";
+#echo "Access Key :$AWS_ACCESS_KEY";
+#echo "Secret Access Key :$AWS_SECRET_ACCESS_KEY";
+
